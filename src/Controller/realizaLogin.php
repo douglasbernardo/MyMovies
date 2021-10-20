a7bc0b515ceeb601225c088a13488603f71b0128
Douglas Bernardo de Melo <douglasbernardomelo@gmail.com>
2021-10-20 14:16:17 -0300

    logar o usuario

--- src/Controller/realizaLogin.php
+++ src/Controller/realizaLogin.php
@@ -3,55 +3,42 @@
 namespace DouglasBernardo\MyMovies\Controller;
 
 use DouglasBernardo\MyMovies\Entity\Usuario;
-use DouglasBernardo\MyMovies\Infra\Conexao;
-use DouglasBernardo\MyMovies\Infra\Queries;
+use DouglasBernardo\MyMovies\Helper\FlashMessages;
 
 class realizaLogin implements Requisicao
 {
 
-    public function __construct()
-    {
-        $this->db = new Queries();
-    }
+    use FlashMessages;
 
     public function handle(): void
     {
-        $dados = [
-            "email" => filter_input(INPUT_POST,'email',FILTER_VALIDATE_EMAIL),
-            "senha" => filter_input(INPUT_POST,'senha',FILTER_SANITIZE_STRING)
-        ];
+        if(isset($_POST['email']) && !empty($_POST['email'])){
+            $email = filter_input(INPUT_POST,'email',FILTER_VALIDATE_EMAIL);
+            $senha = filter_input(INPUT_POST,'senha',FILTER_SANITIZE_STRING);
 
-        if (is_null($dados['email']) || $dados['email'] === false){
-            echo "Digite um email valido";
-            //header('location: /login');
-            return;
+            if (is_null($email) || $email === false){
+                $this->defineMensagem("danger","Digite um email valido");
+                header('location: /login');
+                return;
+            }
+
+            if (is_null($senha) || $senha == ""){
+                $this->defineMensagem("danger","Digite uma senha valida");
+                header('location: /login');
+                return;
+            }
+
+            $usuario = new Usuario();
+
+            if(!$usuario->usuarioExiste($email)){
+                $this->defineMensagem("danger","E-mail ou senha invalidos");
+                header('location: /login');
+                return;
+            }
+            else{
+                $usuario->Logar($email,$senha);
+            }
         }
 
-        if (is_null($dados['senha']) || $dados['senha'] == ""){
-            echo "Digite uma senha valida";
-            //header('location: /login');
-            return;
-        }
-
-        $usuario = new Usuario();
-
-        $sql = "SELECT nome,email FROM usuarios WHERE email = :email and senha = :senha";
-        $sql = $this->db->prepare($sql);
-        $sql->bindValue(":email",$dados['email']);
-        $sql->bindValue(":senha",$dados['senha']);
-        $sql->execute();
-
-        if($sql->rowCount() > 0){
-            $dados = $sql->fetch();
-            
-            echo $dados['id'];
-        }
-        $_SESSION['usuario_logado'] = true;
-
-
-        header("Location: /home");
-
-
-    
     }
 }
