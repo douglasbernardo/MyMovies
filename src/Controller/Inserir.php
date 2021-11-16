1bf701ca59a5ba7cf2ff89c9b684ef0d29722e61
Douglas Bernardo de Melo <douglasbernardomelo@gmail.com>
2021-11-15 23:03:29 -0300

    traduzindo o nome da serie, inserir no banco

--- src/Controller/Inserir.php
+++ src/Controller/Inserir.php
@@ -2,29 +2,59 @@
 
 namespace DouglasBernardo\MyMovies\Controller;
 
+use DouglasBernardo\MyMovies\Helper\FlashMessages;
+use DouglasBernardo\MyMovies\Helper\Template;
 use DouglasBernardo\MyMovies\OMDB\Client;
 use Statickidz\GoogleTranslate;
 
 class Inserir
 {
+
+    use FlashMessages;
+    use Template;
+
     public function handle(): void
     {   
     
         $client = new Client("172da14f");
 
-        $movieName = "Squid Game";
-
+       $movieName = filter_input(INPUT_POST,'nome',FILTER_SANITIZE_STRING);
+       $opcao = filter_input(INPUT_POST,'options',FILTER_SANITIZE_STRING);
+        
         $translator = new GoogleTranslate();
-        $result = $translator->translate("pt","en",$movieName); //traduz o filme do portugues para o ingles
-
+        $movieTranslated = $translator->translate("pt","en",$movieName); //traduz o filme do portugues para o ingles
 
         $movie = $client->searchOneMovie(
-            urlencode($result),
-           // "series"
+            urlencode($movieTranslated),
+            $opcao
         );
 
-       echo "<pre>";var_dump($movie);
+        if($movie->Poster === "N/A") //se a imagem do filme for NOT FOUND
+        {
+            $this->defineMensagem("danger","A imagem do filme não foi encontrado na nossa Api, faça o download de uma e a adicione");
+            header("Location: /insercao");
+            return;
+        }
 
-       echo "<img src='$movie->Poster'>";
+        if($movie->Response === "False") //se a resposta for == "False" é porque o filme não foi encontrado
+        {
+            $this->defineMensagem("danger","Não conseguimos encontrar esse filme na nossa Api");
+            header("Location: /insercao");
+            return;
+        }
+        
+        // echo "<pre>";
+        //   var_dump(
+        //      $movie
+        //      // $movie->Title,
+        //      // $movie->Poster
+        //   );
+        // echo "</pre>";
+
+
+        //Mostra o link da imagem do filme que o usuario escolheu
+        echo $this->renderizaHtml('insercao/formulario',[
+            "imagem" => $movie->Poster
+        ]);
     }
 }
