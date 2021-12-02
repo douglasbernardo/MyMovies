24c700a4e9c99a5778801564025ddb5aea820d42
Douglas Bernardo de Melo <douglasbernardomelo@gmail.com>
2021-12-02 17:28:36 -0300

    inserir no banco

--- src/Controller/Inserir.php
+++ src/Controller/Inserir.php
@@ -2,9 +2,13 @@
 
 namespace DouglasBernardo\MyMovies\Controller;
 
+use DouglasBernardo\MyMovies\Entity\Filme;
 use DouglasBernardo\MyMovies\Entity\Imagem;
+use DouglasBernardo\MyMovies\Entity\Serie;
 use DouglasBernardo\MyMovies\Helper\FlashMessages;
 use DouglasBernardo\MyMovies\Helper\Template;
+use Error;
+use Exception;
 
 class Inserir
 {
@@ -16,6 +20,8 @@ class Inserir
 
         $nome = filter_input(INPUT_POST,'nome',FILTER_SANITIZE_STRING);
         $opcao = filter_input(INPUT_POST,'options',FILTER_SANITIZE_STRING);
+        $qtdTemporadas = filter_input(INPUT_POST,'qtdTemporadas',FILTER_VALIDATE_INT);
+        $qtdEpisodios = filter_input(INPUT_POST,'qtdEpisodios',FILTER_VALIDATE_INT);
         $opiniao = filter_input(INPUT_POST,'opiniao',FILTER_SANITIZE_STRING);
         $nota = filter_input(INPUT_POST,'minhaNota',FILTER_VALIDATE_FLOAT);
 
@@ -41,27 +47,38 @@ class Inserir
             return;
         }
 
-        if(strlen($opiniao) < 10 || strlen($opiniao) > 200){
+        if(strlen($opiniao) < 10 || strlen($opiniao) > 280){
             $this->defineMensagem("danger","Opinião deve ter entre 10 e 200 caracteres");
             header("Location: /insercao");
             return;
         }
 
-        echo "Nome do Filme: $nome", "<br>",
-            "Filme ou Serie :$opcao", "<br>",
-            $imgem->getImageName(), "<br>",
-            "Minha Opinião:$opiniao", "<br>",
-            "Minha Nota: $nota"; "<br>";
-        
+        switch($opcao){
+            case "movie":
+                $filme = new Filme();
+                $filme->setNome($nome);
+                $filme->setNota($nota);
+                $filme->setOpiniao($opiniao);
+    
+                echo "<pre>";var_dump($filme); echo "</pre>";
+                
+                break;
+            
+            case "series":
+                $serie = new Serie();
+                $serie->setNome($nome);
+                $serie->setTemporadas($qtdTemporadas);
+                $serie->setEpisodios($qtdEpisodios);
+                $serie->setNota($nota);
+                $serie->setOpiniao($opiniao);
 
-        // if($opcao === "movie"){
-        //     echo "Inserir na tabela de filmes";
-        // }
+                echo "<pre>";var_dump($serie); echo "</pre>";
 
-        // if($opcao === "series"){
-        //     echo "Inserir na tabela de series";
-        // }
+                break;
 
-
+            default:
+                //throw new Exception("Opção não encontrada");
+                break;
+        }
     }
 }
