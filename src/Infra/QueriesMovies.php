9890f2665db0209b3a68bf6d0202eb772d2d7531
Douglas Bernardo de Melo <douglasbernardomelo@gmail.com>
2021-12-07 20:04:32 -0300

    adicionar series e filmes no bd

--- src/Infra/QueriesMovies.php
+++ src/Infra/QueriesMovies.php
@@ -10,19 +10,10 @@ class QueriesMovies
 {
     use Conexao;
 
-    public function insertMovie($nome,$imagem,$nota,$opiniao,$id_user){
-        // $dados = array_keys($values);
-        // $campos = array_pad([],count($dados),'?');
-        // $con = $this->con->prepare('INSERT INTO filmes SET id_usuario = '.$id_user.' ('.implode(',',$dados).') VALUES ('.implode(',',$campos));
-        
-        // // print_r($con);
+    public function insertMovie($nome,$imagem,$nota,$opiniao){
 
-        // // return;
-        // $con->execute(array_values($values));
+        $this->con->query("INSERT INTO filmes SET id_usuario = '".$_SESSION['usuario_id']."', nome = '$nome', imagem = '$imagem', nota = '$nota', opiniao = '$opiniao'");
 
-        $this->con->query("INSERT INTO filmes SET id_usuario = '".$id_user."', nome = '$nome', imagem = '$imagem', nota = '$nota', opiniao = '$opiniao'");
-
-      //  return $this->con->lastInsertId();
     }
 
     // public function insertSeries($values){
@@ -32,20 +23,6 @@ class QueriesMovies
     //     $con->execute(array_values($values));
 
     //     return $this->con->lastInsertId();
-    // }
-
-    // public function select($email,$senha){
-    //     $sql = "SELECT email,senha FROM usuarios WHERE email = :email and senha = :senha";
-    //     $sql = $this->con->prepare($sql);
-    //     $sql->bindValue(":email",$email);
-    //     $sql->bindValue(":senha",$senha);
-    //     $sql->execute();
-
-    //     if($sql->rowCount() > 0){
-    //         $dados = $sql->fetch();
-            
-    //         echo $dados['id'];
-    //     }
     // }
 
     public function selectAllFromMovies(){
