01de74ec5e9c13e40e3f8f30c8e71c93c433fbb4
Douglas Bernardo de Melo <douglasbernardomelo@gmail.com>
2021-12-07 21:02:51 -0300

    ver todos os filmes e series cadastrados

--- view/home.php
+++ view/home.php
@@ -9,18 +9,20 @@
     <div class="carrosel-desenhos">
         <div class="owl-carousel owl-theme">
             <?php foreach($filmes as $filme):?>
-                <div class="item"><img class="box-desenho w-75" src="assets/images/uploads/<?php echo $filme->imagem ?>" alt="<?php echo $filme->nome ?>"></div>
+                <div class="item">
+                    <img class="box-desenho w-75" src="assets/images/uploads/<?php echo $filme->imagem ?>" alt="<?php echo $filme->nome ?>">
+                </div>
             <?php endforeach ?>
         </div>
     </div>
-    <div class="carrosel-desenhos">
+    <!-- <div class="carrosel-desenhos">
         <div class="owl-carousel owl-theme">
             <div class="item"><img class="box-desenho w-75" src="assets/images/round6.jpg" alt="Round6"></div>
             <div class="item"><img class="box-desenho w-75" src="assets/images/st.jpeg" alt="Stranger Things"></div>
             <div class="item"><img class="box-desenho w-75" src="assets/images/riverdale.jpg" alt="Os Flintstones"></div>
             <div class="item"><img class="box-desenho w-75" src="assets/images/la.jpg" alt="Johny O Bravo"></div>
             <div class="item"><img class="box-desenho w-75" src="assets/images/sexEducation.jpg" alt="Turma Do Pateta"></div>
-            <div class="item"><img class="box-desenho w-75" src="assets/images/sabrina.jpg" alt="Meninas Super Poderosas"></div>
+            <div class="item"><img class="box-desenho w-75" src="assets/images/sabrina.jpg" alt="Meninas Super Poderosas"></div> 
         </div>
-    </div>
+    </div> -->
 <?php require_once __DIR__ . './finalHtml.php' ?>
