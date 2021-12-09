2112bc5c77097a5bd964f15f8214455173cd931a
Douglas Bernardo de Melo <douglasbernardomelo@gmail.com>
2021-12-08 21:25:06 -0300

    final do codigo html

--- view/finalHtml.php
+++ view/finalHtml.php
@@ -3,8 +3,6 @@
         <script src="assets/js/codeJs/ChooseOptions.js"></script>
         <script src="assets/js/codeJs/localStorage.js"></script>
         <script src="assets/js/codeJs/SearchImage.js"></script>
-        <script src="assets/js/owl/owl.carousel.min.js"></script>
         <script src="https://kit.fontawesome.com/2c36e9b7b1.js"></script>
-        <script src="assets/js/setup.js"></script>
     </body>
 </html>
