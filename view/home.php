1157b4478000d49df55077c0bbfdd11902bd2f34
Douglas Bernardo de Melo <douglasbernardomelo@gmail.com>
2021-12-08 21:14:07 -0300

    nova visualização

--- view/home.php
+++ view/home.php
@@ -6,23 +6,24 @@
             Inserir um filme ou série
         </button>
     </a>
-    <?php foreach($filmes as $filme):?>
-    <div class="card" style="width: 18rem;">
-            <img src="assets/images/uploads/<?= $filme->imagem ?>" class="card-img-top" alt="...">
-            <div class="card-body">
+    <div class="card-images">
+        <?php foreach($filmes as $filme):?>
+        <div class="card" style="border-radius: 15%;">
+            <img src="assets/images/uploads/<?= $filme->imagem ?>" style="border-radius: 15%;" class="card-img-top" alt="...">
+            <!-- <div class="card-body">
                 <h5 class="card-title">Card title</h5>
                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                 <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>
+            </div> -->
         </div>
-    <?php endforeach ?>
+        <?php endforeach ?>
+    </div>
     <!-- <div class="carrosel-desenhos">
         <div class="owl-carousel owl-theme">
-            <?php foreach($filmes as $filme):?>
+        
                 <div class="item">
                     <img class="box-desenho w-75" src="assets/images/uploads/<?= $filme->imagem ?>" alt="<?php echo $filme->nome ?>">
                 </div>
-            <?php endforeach ?>
         </div>
     </div> -->
 
