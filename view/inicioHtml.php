216cc4ba73adda02f0053f160d1a1c6b438378b8
Douglas Bernardo de Melo <douglasbernardomelo@gmail.com>
2021-12-08 21:23:54 -0300

    inicio do codigo html

--- view/inicioHtml.php
+++ view/inicioHtml.php
@@ -6,9 +6,6 @@
         <link rel="stylesheet" type="text/css" href="assets/estilos/css.css">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" 
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-        <link rel="stylesheet" type="text/css" href="assets/estilos/owl/owl.carousel.min.css">
-        <link rel="stylesheet" type="text/css" href="assets/estilos/owl/owl.theme.default.min.css">
-        <link rel="stylesheet" type="text/css" href="assets/estilos/responsive.css">
         <link name="viewport" content="width=device-width">
     </head>
     <body style="background-color: black;">
