2b782547ad55046512e2e8029e5d78ddf45a60e5
Douglas Bernardo de Melo <douglasbernardomelo@gmail.com>
2021-12-13 20:15:41 -0300

    pagina principal

--- view/home.php
+++ view/home.php
@@ -4,20 +4,18 @@ require_once __DIR__ . './inicioHtml.php' ?>
 
 
     <a href="/insercao">
-        <button class="btn btn-warning mt-4 mb-3 m-3">
+        <button class="btn btn-outline-primary mt-4 mb-3 m-3">
             Inserir um filme ou série
         </button>
     </a>
     <div class="images-container">
         <?php foreach($filmes as $filme):?>
         <div class="filmes">
-            <a href="#">
-                <?php 
-                    echo $filme->imagem == "" ? 
-                        "<a href='/show?id=$filme->id'><img class='card-img-top' src='assets/images/noImage.png' alt='$filme->nome'></a>" : 
-                        "<a href='/show?id=$filme->id'><img class='card-img-top' src='assets/images/uploads/$filme->imagem' alt='$filme->nome'></a>";
-                ?>
-            </a>
+            <?php 
+                echo $filme->imagem == "" ? 
+                    "<a href='/show?id=$filme->id'><img class='card-img-top' src='assets/images/noImage.png' alt='$filme->nome'></a>" : 
+                    "<a href='/show?id=$filme->id'><img class='card-img-top' src='assets/images/uploads/$filme->imagem' alt='$filme->nome'></a>";
+            ?>
         </div>
         <?php endforeach ?>
     </div>
