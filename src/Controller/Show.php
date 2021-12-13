692301e68c2f5cad5d1fea39d1e77c456c844239
Douglas Bernardo de Melo <douglasbernardomelo@gmail.com>
2021-12-13 20:13:28 -0300

    renderiza o filme para mostrar seus detalhes

---
+++ src/Controller/Show.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace DouglasBernardo\MyMovies\Controller;
+
+use DouglasBernardo\MyMovies\Controller\Requisicao;
+use DouglasBernardo\MyMovies\Helper\Template;
+use DouglasBernardo\MyMovies\Infra\QueriesMovies;
+
+class Show implements Requisicao
+{
+
+    use Template;
+
+    public function __construct()
+    {
+     $this->db  = new QueriesMovies();   
+    }
+
+    public function handle(): void
+    {
+        $filme = $this->db->selectOneMovie($_GET['id']);
+
+        echo $this->renderizaHtml("show/about",[
+            "filme" => $filme
+        ]);
+    }
+}
