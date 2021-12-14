08c869695440ebad2b9b310f5313c541e1de7bcf
Douglas Bernardo de Melo <douglasbernardomelo@gmail.com>
2021-12-14 20:33:52 -0300

    deletar conta do usuario

--- src/Controller/DeleteAccount.php
+++ src/Controller/DeleteAccount.php
@@ -9,13 +9,13 @@ class DeleteAccount implements Requisicao
     public function __construct()
     {
         $this->db = new QueriesUsers();   
-
     }
 
     public function handle(): void
     {
         $this->db->deleteUserMovieAccount($_SESSION['usuario_id']);
         $this->db->deleteUserAccount($_SESSION['usuario_id']);
+
         session_destroy();
 
         header('location:/cadastro');
